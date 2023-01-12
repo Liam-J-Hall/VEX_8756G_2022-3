1282cd5c86d2804db5e61e39961c554cdbc46255
Karth <33555279+Oddcheetah2013@users.noreply.github.com>
2023-01-12 15:24:45 -0600

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,217 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       VEX                                                       */
+/*    Created:      Thu Sep 26 2019                                           */
+/*    Description:  Competition Template                                      */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// motor_a              motor         1               
+// Controller1          controller                    
+// motor_b              motor         2               
+// motor_c              motor         3               
+// motor_d              motor         4               
+// flywheel_1           motor         6               
+// flywheel_2           motor         7               
+// intake_1             motor         8               
+// intake_2             motor         9               
+// odometry_rotation    rotation      10              
+// odometry_distance    distance      11              
+// inertial             inertial      5               
+// pneumatics           digital_out   A               
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
 #include "vex.h"
-#include "robot-config.cpp" // !! REMOVE IF NECESARY !!
 
 using namespace vex;
 
 // A global instance of competition
 competition Competition;
+double pi = 3.1415926535;
 
 // define your global instances of motors and other devices here
+
+/*---------------------------------------------------------------------------*/
+/*                          Pre-Autonomous Functions                         */
+/*                                                                           */
+/*  You may want to perform some actions before the competition starts.      */
+/*  Do them in the following function.  You must return from this function   */
+/*  or the autonomous and usercontrol tasks will not be started.  This       */
+/*  function is only called once after the V5 has been powered on and        */
+/*  not every time that the robot is disabled.                               */
+/*---------------------------------------------------------------------------*/
+
+double radians_to_degrees(double radians){
+  return radians * 180 / pi;
+}
+
+int bang_bang_motor_controller(){
+  
+  if (flywheel_r.velocity(rpm) < undershoot_target) {
+    flywheel_voltage += 0.2;
+  } else if (flywheel_r.velocity(rpm) > overshoot_target) {
+    flywheel_voltage -= 0.;
+  }
+
+  return 0;
+}
 
 void pre_auton(void) {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  flywheel_1.setBrake(coast);
-  flywheel_2.setBrake(coast); 
+  
+  Brain.Screen.clearScreen();
+  GPS.calibrate();
+  task bang_bang = task(bang_bang_motor_controller);
+
+  // All activities that occur before the competition starts
+  // Example: clearing encoders, setting servo positions, ...
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              Autonomous Task                              */
+/*                                                                           */
+/*  This task is used to control your robot during the autonomous phase of   */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+
+
+double y_distance;
+double x_distance;
+double y_rotations;
+double x_rotations;
+double r_rotations;
+double motor_a_c_vel;
+double motor_d_b_vel;
+
+/*void MoveAuton (double velocity_auton, double angle_auton, double distance_auton)
+{ 
+  // angle must be given in radians, velocity is given in percentage
+
+  // the distance that each axis has to push the robot
+  y_distance = distance_auton * sin(angle_auton);
+  x_distance = distance_auton * cos(angle_auton);
+  Brain.Screen.print(y_distance);
+  Brain.Screen.print("|");
+
+  // how many times the wheels have to rotate to move y_distance or x_distance
+  y_rotations = y_distance / 4*pi;
+  x_rotations = x_distance / 4*pi;
+  Brain.Screen.print(y_rotations);
+  Brain.Screen.print("|");
+
+  // determine the magnatude of velocity applied by the front two wheels
+  motor_a_c_vel = velocity_auton * sin(angle_auton); // wheel axis 1
+  motor_d_b_vel = velocity_auton * cos(angle_auton); // wheel axis 2
+  Brain.Screen.print(motor_a_c_vel);
+  Brain.Screen.print("|");
+
+  // sets the velocities of the wheels to the magnatudes of the velocities
+  motor_a.setVelocity(motor_a_c_vel, percent);
+  motor_b.setVelocity(motor_d_b_vel, percent);
+  motor_c.setVelocity(motor_a_c_vel, percent);
+  motor_d.setVelocity(motor_d_b_vel, percent);
+  Brain.Screen.print("MotorVel Set|");
+
+  // spin the wheels in the angular velocities. 
+  motor_a.rotateFor(forward, y_rotations * 360, degrees, false); // 2*pi multiplied by the number of 360 degree rotations
+  motor_b.rotateFor(reverse, x_rotations * 360, degrees, false);
+  motor_c.rotateFor(reverse, y_rotations * 360, degrees, false);
+  motor_d.rotateFor(forward, x_rotations * 360, degrees, true);
+  Brain.Screen.print("Moved");
+}*/
+
+/*
+ADHD Fidget Junk Typing
+
+as;lkdjfas;lkdjfsa;ldkfjasldfs;ladfkjas;dlfkjasldfkjasldkjfasldkjfa;sldkjfas;lkdjfas;lkdjfas;ldkfjas;ldkjf;alsdkfja;lsdkfjas;ldjf
+sald;kfjas;ldkfjas;ldkfjas;lkdjfa;sldkjfa;slkdfja;sldkfjas;ldkfjas;ldkfjas;ldkfja;lkjfas;lkdjf;asldkfja;sldkfja;sldkfj
+a;lskdjf;alskdjf;alskdjf;alskdjf;alskdjf;alskdjf
+as;ldkfja;sldkfjasd;flaksjd;lfkasldkfja;sldkfja;sldkfja;sldkfja;sldkfja;sldkfja;sldkfja;sldkfja;sldkfja;sldkfja;lskdjf;alskdjf
+a;lsdkjfa;sldfjka;lskdjf;alskdjf;alskdjf;alskdjf;alskdjf;alskdjf;alskdjf;alskdjf;alskdjf;lskdjf;alskdjf
+*/
+
+double determine_angle_sign(double a_const_input){
+  double a_const_output;
+
+  if (GPS.heading() > 180 && GPS.heading() < 361){
+    a_const_output = -1 * a_const_input;
+  } else {
+    a_const_output = 1 * a_const_input;
+  }
+
+  return a_const_output;
+}
+
+void move_auton(double x_pos, double y_pos, double speed)
+{
+  // calculate angle that the robot must move to get from x_0 to x (and y_0 to y)
+  double delta_y = y_pos - GPS.yPosition();
+  double delta_x = x_pos - GPS.xPosition();
+  double travel_angle = atan(delta_y / delta_x) + pi/2;
+  double axis_1_speed = speed * sin(travel_angle);
+  double axis_2_speed = speed * cos(travel_angle);
+  
+  while ((GPS.xPosition() <= x_pos + 15 && GPS.xPosition() >= x_pos + 15) && (GPS.yPosition() <= y_pos + 15 && GPS.yPosition() >= y_pos -15))
+  {
+    motor_a.spin(fwd, axis_1_speed, percent);
+    motor_b.spin(reverse, axis_2_speed, percent);
+    motor_c.spin(reverse, axis_1_speed, percent);
+    motor_d.spin(fwd, axis_2_speed, percent);
+
+    wait(20, msec);
+  }
+
+  motor_a.stop();
+  motor_b.stop();
+  motor_c.stop();
+  motor_d.stop();
 }
 
 void autonomous(void) {
-  // power flywheel
-  flywheel_velocity = 75;
+  // ..........................................................................
+  // Insert autonomous user code here.
+  // ..........................................................................
+  
+  //motor_a.spin(fwd);
+
+  move_auton(100, 100, 10);
+
+  // Power Flywheel
+  //flywheel_1.spin(forward, 65, percent);
+  //flywheel_2.spin(reverse, 65, percent);
+
+  // Move to roller
+  //move_auton(1800, 1800, 65);
+
+  // Use Roller
+  //intake_2.spinFor(forward, 30, degrees);
+  
+  // Shoot disc
+
+  //indexer.open();
+  //wait(20, msec);
+  //indexer.close();
+
+
+   // power flywheel
+  /*flywheel_velocity = 75;
   flywheel_1.spin(forward, flywheel_velocity, percent);
   flywheel_2.spin(reverse, flywheel_velocity, percent); 
   flywheel_powered = true;
 
   // move to firing position
-  MoveAuton(100, pi/2, 0, 33); // max velocity, 90 degrees, no rotation, 33 inches
+  MoveAuton(100, pi/4, 33); // max velocity, 90 degrees, 33 inches
 
   // rotate towards goal
-  MoveAuton(100, 0, pi/2, 12); // !! LOOK INTO THE RADIUS OF ROBOT !!
+  TurnAuton(pi/4);
 
   // shoot 2 preloads
   for (int i=0; i<2; i++){
@@ -43,12 +228,15 @@ void autonomous(void) {
   //turn on intake
   intake.spin(forward, 65, percent);
   
-  // move towards the stack of discs (3)
-  // collect stack of discs (3)
-  MoveAuton(0,0,0,0); // !! CHANGE NUMBERS !!
+  // move towards the stack of discs (3); collect discs (3);
+  TurnAuton(pi);
+  MoveAuton(100, pi/4, 12); 
+  //wait for discs to be collected
+  wait(100 ,msec);
 
   // move to firing position
-  MoveAuton(0,0,0,0); // !! CHANGE NUMBERS !!
+  MoveAuton(100, pi/4, 12); 
+  TurnAuton(pi);
 
   // fire all stored discs (3) consecutively towards the goal
   for (int i=0; i<3; i++){
@@ -61,6 +249,38 @@ void autonomous(void) {
     //wait 20 milliseconds
     wait(20, msec);
   }
+
+  // turn in orientation to use the far roller
+  TurnAuton(-(3*pi/4));
+
+  // drive to roller
+  MoveAuton(100, atan(1.75/5), sqrt(5*5 + 1.75*1.75));
+
+  // use roller
+  intake.spinTo(radians_to_degrees(2*pi), degrees);*/
+
+}
+
+/*---------------------------------------------------------------------------*/
+/*                                                                           */
+/*                              User Control Task                            */
+/*                                                                           */
+/*  This task is used to control your robot during the user control phase of */
+/*  a VEX Competition.                                                       */
+/*                                                                           */
+/*  You must modify the code to add your own robot specific commands here.   */
+/*---------------------------------------------------------------------------*/
+extern void Move()
+{
+  int motor_a_vel = Controller1.Axis3.position() + Controller1.Axis4.position() + Controller1.Axis1.position();
+  int motor_b_vel = -Controller1.Axis3.position() + Controller1.Axis4.position() + Controller1.Axis1.position();
+  int motor_c_vel = -Controller1.Axis3.position() - Controller1.Axis4.position() + Controller1.Axis1.position();
+  int motor_d_vel = Controller1.Axis3.position() - Controller1.Axis4.position() + Controller1.Axis1.position();
+
+  motor_a.spin(forward, motor_a_vel, percent);
+  motor_b.spin(forward, motor_b_vel, percent);
+  motor_c.spin(forward, motor_c_vel, percent);
+  motor_d.spin(forward, motor_d_vel, percent);
 }
 
 void usercontrol(void) {
@@ -69,6 +289,15 @@ void usercontrol(void) {
     // This is the main execution loop for the user control program.
     // Each time through the loop your program should update motor + servo
     // values based on feedback from the joysticks.
+
+  if (Competition.isDriverControl() == true ){
+    Move();
+  }
+
+    // ........................................................................
+    // Insert user code here. This is where you use the joystick values to
+    // update your motors, etc.
+    // ........................................................................
 
     wait(20, msec); // Sleep the task for a short amount of time to
                     // prevent wasted resources.
--- robot-config.cpp
+++ robot-config.cpp
@@ -1,133 +1,113 @@
 #include "vex.h"
 
 using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
 
-// A global instance of brain used for printing to the V5 brain screen
-brain Brain;
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
 // VEXcode device constructors
+motor motor_a = motor(PORT1, ratio18_1, false);
 controller Controller1 = controller(primary);
-//driving motors
-motor motor_a = motor(PORT1, ratio18_1, false); 
-motor motor_b = motor(PORT9, ratio18_1, false);
-motor motor_c = motor(PORT10, ratio18_1, false);
-motor motor_d = motor(PORT2, ratio18_1, false);
-//other motors
-motor flywheel_1 = motor(PORT7, ratio18_1, false); 
-motor flywheel_2 = motor(PORT8, ratio18_1, false); 
-motor intake = motor(PORT11, ratio18_1, false); 
-//sensors
-rotation odometry_encoder = rotation(PORT5, false);
-distance odometry_distance = distance(PORT20);
-inertial inertia_sensor = inertial(PORT6);
-//pneumatics
-digital_out indexer = digital_out(Brain.ThreeWirePort.A);
-// flywheel stuff
-bool flywheel_powered = false;
-int flywheel_velocity = 0;
-// pi
-float pi = 3.1415926535;
+motor motor_b = motor(PORT2, ratio18_1, false);
+motor motor_c = motor(PORT3, ratio18_1, false);
+motor motor_d = motor(PORT4, ratio18_1, false);
+motor flywheel_1 = motor(PORT6, ratio18_1, false);
+motor flywheel_2 = motor(PORT7, ratio18_1, false);
+motor intake_1 = motor(PORT8, ratio18_1, false);
+motor intake_2 = motor(PORT9, ratio18_1, true);
+rotation odometry_rotation = rotation(PORT12, false);
+distance odometry_distance = distance(PORT11);
+pneumatics indexer = pneumatics(Brain.ThreeWirePort.A);
+rotation flywheel_r = rotation (PORT13, false);
+gps GPS = gps(PORT10, 130.00, 135.00, mm, 90);
 
+// VEXcode generated functions
 // define variable for remote controller enable/disable
 bool RemoteControlCodeEnabled = true;
-bool DrivetrainNeedsToBeStopped_Controller1 = true;
+// define variables used for controlling motors based on controller inputs
+bool Controller1LeftShoulderControlMotorsStopped = true;
+bool Controller1RightShoulderControlMotorsStopped = true;
 
-double radians_to_degrees(float radians){
-  double degrees = radians / pi * 180;
-  return degrees;
-}
+float motor_speed = 85;
+float flywheel_voltage = 10; // 10 VOLTS AT THE MOST!
+double overshoot_target = 11; // 11 VOLTS AT THE MOST!
+double undershoot_target = 9; // 9 VOLTS AT THE MOST!
+bool spin_flywheel;
 
-void Move(){
-  int motor_a_vel = Controller1.Axis3.position() + Controller1.Axis4.position() + Controller1.Axis1.position();
-  int motor_b_vel = -Controller1.Axis3.position() + Controller1.Axis4.position() + Controller1.Axis1.position();
-  int motor_c_vel = -Controller1.Axis3.position() - Controller1.Axis4.position() + Controller1.Axis1.position();
-  int motor_d_vel = Controller1.Axis3.position() - Controller1.Axis4.position() + Controller1.Axis1.position();
-
-  motor_a.setVelocity(motor_a_vel, percent);
-  motor_b.setVelocity(motor_b_vel, percent);
-  motor_c.setVelocity(motor_c_vel, percent);
-  motor_d.setVelocity(motor_d_vel, percent);
-
-  motor_a.spin(forward);
-  motor_b.spin(forward);
-  motor_c.spin(forward);
-  motor_d.spin(forward);
-}
-
-void MoveAuton (float velocity_auton, float angle_auton, float rotation_auton, float distance_auton){ 
-  // angle must be given in radians, velocity is given in percentage
-
-  // the distance that each axis has to push the robot
-  double y_distance = distance_auton * sin(angle_auton);
-  double x_distance = distance_auton * cos(angle_auton);
-
-  // how many times the wheels have to rotate to move y_distance or x_distance
-  double y_rotations = y_distance / 4*pi;
-  double x_rotations = x_distance / 4*pi;
-
-  // determine the magnatude of velocity applied by the front two wheels
-  double motor_a_c_vel = velocity_auton * sin(angle_auton); // wheel axis 1
-  double motor_b_d_vel = velocity_auton * cos(angle_auton); // wheel axis 2
-
-  // sets the velocities of the wheels to the magnatudes of the velocities
-  motor_a.setVelocity(motor_a_c_vel + rotation_auton, percent);
-  motor_b.setVelocity(motor_b_d_vel + rotation_auton, percent);
-  motor_c.setVelocity(-motor_a_c_vel - rotation_auton, percent);
-  motor_d.setVelocity(-motor_b_d_vel - rotation_auton, percent);
-
-  // spin the wheels in the angular velocities. 
-  motor_a.spinTo((radians_to_degrees(2 * pi * y_rotations)), degrees);
-  motor_b.spinTo((radians_to_degrees(2 * pi * x_rotations)), degrees);
-  motor_c.spinTo(-(radians_to_degrees(2 * pi * y_rotations)), degrees);
-  motor_d.spinTo(-(radians_to_degrees(2 * pi * x_rotations)), degrees);
-}
-
-int rc_auto_loop_function_Controller1() {
-  // process the controller input every 20 milliseconds
-  // update the motors based on the input values
-
-  while(true) {
-    if(RemoteControlCodeEnabled) {
-      // calculate the drivetrain motor velocities from the controller joystick axies
-      Move();
-      // spin flywheel at the velocity set by input
-      flywheel_1.spin(forward, flywheel_velocity, percent); 
-      flywheel_2.spin(reverse, flywheel_velocity, percent); 
-      
-      // power flywheel with B
-      if (Controller1.ButtonB.pressing()) {
-        if(flywheel_powered == true){
-          // turns off flywheel
-          flywheel_powered = false;
-          // lower flywheel velocity
-          flywheel_velocity = 0;
-        } else {
-          // turns on flywheel
-          flywheel_powered = true;
-          // raises the flywheel velocity
-          flywheel_velocity = 65;
-        }
-      } 
-      
-      // use pneumatics with R1
-      if (Controller1.ButtonR1.pressing()) {
-        //bring flywheel to 85 percent
-        flywheel_velocity = 85;
-        //wait 20 msec
-        wait(20, msec);
-        // push indexer out
-        indexer.set(true);
-        // wait 20 milliseconds
-        wait(20, msec);
-        // pull indexer in
-        indexer.set(false);
-        // reset motor speed
-        flywheel_velocity = 65;
-      }
-    }
+void do_spin_flywheel(){
+  if (spin_flywheel == true){
+    flywheel_1.spin(forward, flywheel_voltage, volt);
+    flywheel_2.spin(reverse, flywheel_voltage, volt);
   }
 }
 
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+
+      Brain.Screen.clearLine();
+
+      // check the ButtonL1/ButtonL2 status to control intake_1
+      if (Controller1.ButtonR1.pressing()) {
+        intake_1.spin(forward, motor_speed, percent);
+        intake_2.spin(forward, motor_speed, percent);
+        Controller1RightShoulderControlMotorsStopped = false;
+      } else if (Controller1.ButtonR2.pressing()) {
+        intake_1.spin(reverse, motor_speed, percent);
+        intake_2.spin(reverse, motor_speed, percent);
+        Controller1RightShoulderControlMotorsStopped = false;
+      } else if (!Controller1RightShoulderControlMotorsStopped) {
+        intake_1.stop();
+        intake_2.stop();
+        // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+        Controller1RightShoulderControlMotorsStopped = true;
+      }
+      
+    if (Controller1.ButtonB.pressing()) 
+    {
+      spin_flywheel = true;
+      
+      Controller1LeftShoulderControlMotorsStopped = false;
+    }  else /*if (!Controller1LeftShoulderControlMotorsStopped) */
+    {
+      spin_flywheel = false;
+      // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+      Controller1LeftShoulderControlMotorsStopped = true;
+    }
+
+    if (Controller1.ButtonL2.pressing()) {
+      indexer.open();
+    } else {
+      indexer.close();
+    }
+
+    /*Brain.Screen.print(flywheel_1.velocity(percent));
+    Brain.Screen.newLine();
+    Brain.Screen.print(flywheel_1.voltage());
+    Brain.Screen.newLine();
+    Brain.Screen.print("");
+    // wait before repeating the process*/
+
+    //Move();
+    do_spin_flywheel();
+
+
+    wait(20, msec);
+    }
+  }
+  return 0;
+}
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
 void vexcodeInit( void ) {
-  // nothing to initialize
   task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
 }
