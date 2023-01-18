1349a674d68fbfe3e4ad475793f49f629c4d3af3
TurtleHacked <lavastar9@gmail.com>
2023-01-18 12:38:45 -0600

    WOOOHOOOO updated code! hasn't been updated in a while

--- src/robot-config.cpp
+++ src/robot-config.cpp
@@ -8,20 +8,17 @@ using code = vision::code;
 brain  Brain;
 
 // VEXcode device constructors
-motor motor_a = motor(PORT1, ratio18_1, false);
 controller Controller1 = controller(primary);
-motor motor_b = motor(PORT2, ratio18_1, false);
-motor motor_c = motor(PORT3, ratio18_1, false);
-motor motor_d = motor(PORT4, ratio18_1, false);
-motor flywheel_1 = motor(PORT6, ratio18_1, false);
-motor flywheel_2 = motor(PORT7, ratio18_1, false);
-motor intake_1 = motor(PORT8, ratio18_1, false);
-motor intake_2 = motor(PORT9, ratio18_1, true);
-rotation odometry_rotation = rotation(PORT12, false);
-distance odometry_distance = distance(PORT11);
+motor motor_a = motor(PORT12, ratio18_1, false);
+motor motor_b = motor(PORT13, ratio18_1, false);
+motor motor_c = motor(PORT14, ratio18_1, false);
+motor motor_d = motor(PORT15, ratio18_1, false);
+motor flywheel_1 = motor(PORT19, ratio18_1, false);
+motor flywheel_2 = motor(PORT20, ratio18_1, false);
+motor intake_1 = motor(PORT17, ratio18_1, false);
+motor intake_2 = motor(PORT18, ratio18_1, true);
 pneumatics indexer = pneumatics(Brain.ThreeWirePort.A);
-rotation flywheel_r = rotation (PORT13, false);
-gps GPS = gps(PORT10, 130.00, 135.00, mm, 90);
+gps GPS = gps(PORT16, 130.00, 135.00, mm, -90);
 
 // VEXcode generated functions
 // define variable for remote controller enable/disable
@@ -30,17 +27,16 @@ bool RemoteControlCodeEnabled = true;
 bool Controller1LeftShoulderControlMotorsStopped = true;
 bool Controller1RightShoulderControlMotorsStopped = true;
 
-float motor_speed = 85;
+float intake_speed = 85;
+double flywheel_mult;
 float flywheel_voltage = 10; // 10 VOLTS AT THE MOST!
 double overshoot_target = 11; // 11 VOLTS AT THE MOST!
 double undershoot_target = 9; // 9 VOLTS AT THE MOST!
 bool spin_flywheel;
 
 void do_spin_flywheel(){
-  if (spin_flywheel == true){
-    flywheel_1.spin(forward, flywheel_voltage, volt);
-    flywheel_2.spin(reverse, flywheel_voltage, volt);
-  }
+  flywheel_1.spin(forward, flywheel_voltage * flywheel_mult, volt);
+  flywheel_2.spin(reverse, flywheel_voltage * flywheel_mult, volt);
 }
 
 // define a task that will handle monitoring inputs from Controller1
@@ -54,12 +50,12 @@ int rc_auto_loop_function_Controller1() {
 
       // check the ButtonL1/ButtonL2 status to control intake_1
       if (Controller1.ButtonR1.pressing()) {
-        intake_1.spin(forward, motor_speed, percent);
-        intake_2.spin(forward, motor_speed, percent);
+        intake_1.spin(forward, intake_speed, percent);
+        intake_2.spin(forward, intake_speed, percent);
         Controller1RightShoulderControlMotorsStopped = false;
       } else if (Controller1.ButtonR2.pressing()) {
-        intake_1.spin(reverse, motor_speed, percent);
-        intake_2.spin(reverse, motor_speed, percent);
+        intake_1.spin(reverse, intake_speed, percent);
+        intake_2.spin(reverse, intake_speed, percent);
         Controller1RightShoulderControlMotorsStopped = false;
       } else if (!Controller1RightShoulderControlMotorsStopped) {
         intake_1.stop();
@@ -70,15 +66,27 @@ int rc_auto_loop_function_Controller1() {
       
     if (Controller1.ButtonB.pressing()) 
     {
-      spin_flywheel = true;
-      
+      flywheel_mult = 0.5;      
       Controller1LeftShoulderControlMotorsStopped = false;
-    }  else /*if (!Controller1LeftShoulderControlMotorsStopped) */
+    }  else if (Controller1.ButtonA.pressing()) 
     {
-      spin_flywheel = false;
+      flywheel_mult = 1.0;
       // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
-      Controller1LeftShoulderControlMotorsStopped = true;
-    }
+      Controller1LeftShoulderControlMotorsStopped = false;
+    } else if (Controller1.ButtonY.pressing()) 
+    {
+      flywheel_mult = 0.75;
+      // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+      Controller1LeftShoulderControlMotorsStopped = false;
+    } else if (Controller1.ButtonX.pressing()) 
+    {
+      flywheel_mult = 0;
+      // set the toggle so that we don't constantly tell the motor to stop when the buttons are released
+      Controller1LeftShoulderControlMotorsStopped = false;
+    } 
+
+    do_spin_flywheel();
+
 
     if (Controller1.ButtonL2.pressing()) {
       indexer.open();
@@ -94,7 +102,6 @@ int rc_auto_loop_function_Controller1() {
     // wait before repeating the process*/
 
     //Move();
-    do_spin_flywheel();
 
 
     wait(20, msec);
